41223fc040eb0d249f532d78b8f816e70a4e9b39
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-29 21:28:22 +0200

    pretty

--- eggtimer_test.py
+++ eggtimer_test.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 
 class Timer:
 
@@ -13,17 +15,24 @@ class Timer:
 
     def pause(self, when):
         self.running = False
-        self.value = max(self.value - when, 0)
+        self.value = self.__left(when)
 
     def left(self, when):
         if(not self.running):
-            return self.value
+            return self.__pretty(self.value)
 
-        result = max(self.value - when, 0)
+        result = self.__left(when)
         if(result == 0):
             notify()
 
-        return result
+        return self.__pretty(result)
+
+    def __left(self, when):
+        return max(self.value - when, 0)
+
+    def __pretty(self, seconds):
+        td = timedelta(seconds=seconds)
+        return str(td)[2:]
 
 
 # Tests
@@ -31,33 +40,31 @@ class Timer:
 
 def test_starts_paused():
     timer = Timer(60)
-    assert timer.left(0) == 60
-    assert timer.left(1) == 60
+    assert timer.left(0) == '01:00'
+    assert timer.left(1) == '01:00'
 
 
 def test_starts_counting_down():
     timer = Timer(60)
     timer.start(0)
-    assert timer.left(1) == 59
-    assert timer.left(2) == 58
-    assert timer.left(60) == 0
-    assert timer.left(61) == 0  # does not overcount
+    assert timer.left(1) == '00:59'
+    assert timer.left(2) == '00:58'
+    assert timer.left(60) == '00:00'
+    assert timer.left(61) == '00:00'  # does not overcount
 
 
 def test_notifies_when_over():
     timer = Timer(60, notify)
     timer.start(0)
-    assert timer.left(60) == 0
+    assert timer.left(60) == '00:00'
     assert notified == 'notified'
 
 
 def test_pauses():
     timer = Timer(60)
     timer.start(0)
-    timer.left(30)
     timer.pause(30)
-
-    assert timer.left(60) == 30
+    assert timer.left(60) == '00:30'
 
 
 notified = ''
