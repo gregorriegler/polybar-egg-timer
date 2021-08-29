94d2710e8a83284c4fee91e14f18de445081918d
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-29 20:45:09 +0200

    finish/notify, pause

--- eggtimer_test.py
+++ eggtimer_test.py
@@ -1,41 +1,68 @@
+
 class Timer:
-    value = 60
-    notify = None
 
-    def __init__(self, notify=None):
+    def __init__(self, value=60, notify=None):
+        self.value = value
         self.notify = notify
-
+        self.running = False
 
     def start(self, when):
-        value = when
+        self.start = when
+        self.running = True
         pass
 
+    def pause(self, when):
+        self.running = False
+        self.value = max(self.value - when, 0)
+
     def left(self, when):
-        result = self.value - when
-        if(result <= 0):
+        if(not self.running):
+            return self.value
+
+        result = max(self.value - when, 0)
+        if(result == 0):
             notify()
+
         return result
 
-# Code
 
-def test_starts_at_60_by_default():
-    timer = Timer()
+# Tests
+
+
+def test_starts_paused():
+    timer = Timer(60)
     assert timer.left(0) == 60
+    assert timer.left(1) == 60
 
-def test_counts_down():
-    timer = Timer()
+
+def test_starts_counting_down():
+    timer = Timer(60)
     timer.start(0)
     assert timer.left(1) == 59
     assert timer.left(2) == 58
+    assert timer.left(60) == 0
+    assert timer.left(61) == 0  # does not overcount
 
-notified = ''
-def notify():
-    global notified
-    notified = 'notified'
 
 def test_notifies_when_over():
-    timer = Timer(notify)
+    timer = Timer(60, notify)
     timer.start(0)
     assert timer.left(60) == 0
     assert notified == 'notified'
 
+
+def test_pauses():
+    timer = Timer(60)
+    timer.start(0)
+    timer.left(30)
+    timer.pause(30)
+
+    assert timer.left(60) == 30
+
+
+notified = ''
+
+
+def notify():
+    global notified
+    notified = 'notified'
