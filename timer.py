8aa49c0654181f34c893cce87d6328bd199937fb
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-29 23:41:46 +0200

    one _ enough

--- timer.py
+++ timer.py
@@ -4,30 +4,30 @@ from datetime import timedelta
 class Timer:
 
     def __init__(self, value=60, notify=None):
-        self.__value = value
-        self.__notify = notify
-        self.__running = False
+        self._value = value
+        self._notify = notify
+        self._running = False
 
     def start(self, when):
-        self.__start = when
-        self.__running = True
+        self._start = when
+        self._running = True
 
     def pause(self, when):
-        self.__running = False
-        self.__value = self.__seconds_left(when)
+        self._running = False
+        self._value = self._seconds_left(when)
 
     def time(self, when):
-        if(not self.__running):
-            return mmss(self.__value)
+        if(not self._running):
+            return mmss(self._value)
 
-        result = self.__seconds_left(when)
-        if(result == 0 and self.__notify):
-            self.__notify()
+        result = self._seconds_left(when)
+        if(result == 0 and self._notify):
+            self._notify()
 
         return mmss(result)
 
-    def __seconds_left(self, when):
-        return max(self.__value - (when - self.__start), 0)
+    def _seconds_left(self, when):
+        return max(self._value - (when - self._start), 0)
 
 
 def mmss(seconds):
