1439ad4b2dc0e4ad69f1eb7d28b09fd2a3fe8e0d
Gregor Riegler <gregor.riegler@ebcont.com>
2021-09-03 20:06:44 +0200

    main class

--- eggtimer.py
+++ eggtimer.py
@@ -8,43 +8,43 @@ from commands import commands
 # seconds multiplier but nonbreaking
 # stdout redirect
 # e2e test
+# need main with asyncio.run ??
 # renames
 # pause vs current in timer
 # no more globals, all classes
 
-async def egg_timer():
-    await asyncio.gather(run_timer(),handle_commands())
+class EggTimerApp:
 
+    def main(self):
+        asyncio.run(self.egg_timer())
 
-async def handle_commands():
-    async for command in commands():
-        print(command)
-        mapping = {
-            'start': start,
-            'pause': pause
-        }
-        mapping.get(command)()
+    async def egg_timer(self):
+        await asyncio.gather(self.run_timer(), self.handle_commands())
 
+    async def handle_commands(self):
+        async for command in commands():
+            print(command)
+            mapping = {
+                'start': self.start,
+                'pause': self.pause
+            }
+            mapping.get(command)()
 
-def start():
-    global timer
-    timer.start(time.time())
+    def start(self):
+        global timer
+        timer.start(time.time())
 
-def pause():
-    global timer
-    timer.pause(time.time())
+    def pause(self):
+        global timer
+        timer.pause(time.time())
 
-async def run_timer():
-    global timer
-    timer = Timer(60, lambda: print("ok"))
-    while True:
-        print(timer.time(time.time()))
-        await asyncio.sleep(1)
-
-
-def main():
-    asyncio.run(egg_timer())
+    async def run_timer(self):
+        global timer
+        timer = Timer(60, lambda: print("ok"))
+        while True:
+            print(timer.time(time.time()))
+            await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    EggTimerApp().main()
