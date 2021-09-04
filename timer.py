d2777ad24f687575b799aade242cd39039fc6392
Gregor Riegler <gregor.riegler@ebcont.com>
2021-09-04 14:17:33 +0200

    renames

--- timer.py
+++ timer.py
@@ -5,8 +5,8 @@ class Timer:
 
     def __init__(self, value=60, notify=None):
         self._value = value
-        self._current = value
         self._notify = notify
+        self._stopped_at = value
         self._running = False
 
     def start(self, timestamp):
@@ -14,12 +14,12 @@ class Timer:
         self._running = True
 
     def pause(self, timestamp):
-        self._current = self._seconds_left(timestamp)
+        self._stopped_at = self._seconds_left(timestamp)
         self._running = False
 
     def time(self, timestamp):
         if(not self._running):
-            return mmss(self._current)
+            return mmss(self._stopped_at)
 
         seconds_left = self._seconds_left(timestamp)
         if(seconds_left == 0):
@@ -29,11 +29,11 @@ class Timer:
         return mmss(seconds_left)
 
     def _seconds_left(self, timestamp):
-        return max(self._current - (timestamp - self._start), 0)
+        return max(self._stopped_at - (timestamp - self._start), 0)
 
     def _reset(self):
         self._running = False
-        self._current = self._value
+        self._stopped_at = self._value
 
     def _notify_over(self):
         if(self._notify):
